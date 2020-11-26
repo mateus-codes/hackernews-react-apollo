9193492aef5d8a0d29ae54975eed7c6a0abeb99f
mateus-codes <mateus.automation@gmail.com>
2020-11-26 16:06:53 -0300

    Routing

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+import { BrowserRouter } from 'react-router-dom'
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -20,9 +22,12 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
